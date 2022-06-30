ce5bbd03d5dda1a2b18b4b82e51617809b4c30dc
Billy Arredondo <billy.arredondo@pucp.pe>
2022-06-30 10:15:21 -0500

    Manage server domain
    Add getUrl method for managing backend server domain

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 
 window.onload = async () => {
-    const response = await fetch('https://opticas-dyc.herokuapp.com/api/aplication');
+    const response = await fetch(`${getUrl()}/api/aplication`);
     const { totalRows, aplications } = await response.json()
     console.log(aplications)
+}
+
+const getUrl = () => {
+    if (document.location.origin.includes('localhost'))
+        return 'http://localhost:2907';
+
+    return 'https://opticas-dyc.herokuapp.com';
 }
