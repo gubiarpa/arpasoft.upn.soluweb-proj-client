86b0713bd00059f8c28bd10add1eac28a1d2fb4e
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-01 21:20:31 -0500

    Contact form style

--- js/components/inicio.component.js
+++ js/components/inicio.component.js
@@ -1,7 +1,17 @@
 const buildInicio = async () => {
-    const { slogan, welcome, customer1, testimony1 } = await getInicioContent();
+    const {
+        slogan,
+        welcome,
+        customer1,
+        testimony1,
+        customer2,
+        testimony2
+    } = await getInicioContent();
+    
     document.querySelector('#_slogan').innerHTML = slogan;
     document.querySelector('#_welcome').innerHTML = welcome;
     document.querySelector('#_customer1').innerHTML = customer1;
     document.querySelector('#_testimony1').innerHTML = testimony1;
+    document.querySelector('#_customer2').innerHTML = customer2;
+    document.querySelector('#_testimony2').innerHTML = testimony2;
 }
