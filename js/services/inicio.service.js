09da952fd86018ef2e28501eb78892792c17c924
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-06 22:10:16 -0500

    Consumption Inicio Endpoint

--- js/services/inicio.service.js
+++ js/services/inicio.service.js
@@ -1,8 +1,8 @@
 
-const PRODUCTS_CONTAINER = '#servicios-container';
-const PRODUCTS_ENDPOINT = 'product';
-
 const loadProducts = async () => {
+
+    const PRODUCTS_CONTAINER = '#servicios-container';
+    const PRODUCTS_ENDPOINT = 'product';
 
     const loadOneProduct = ({ title, content }, index) => {
 
@@ -27,4 +27,18 @@ const loadProducts = async () => {
         container.appendChild(div);
     });
 
+}
+
+const loadTestimonies = async () => {
+    const {
+        customer1,
+        testimony1,
+        customer2,
+        testimony2,
+    } = await getEndpoint('inicio');
+
+    document.querySelector('#_customer1').innerHTML = customer1;
+    document.querySelector('#_testimony1').innerHTML = testimony1;
+    document.querySelector('#_customer2').innerHTML = customer2;
+    document.querySelector('#_testimony2').innerHTML = testimony2;
 }
